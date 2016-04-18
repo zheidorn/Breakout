aaa6b45124cef726c8ab7e03d24b47df69c21163
student3 <student3@jh-mbair-158-10.d214.org>
2016-04-18 14:47:55 -0500

    ds

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    
+    var dynamicAnimator = UIDynamicAnimator()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: view)
+
+    }
+    
+    func setUpViews(){
+        let blueSquare = UIView(frame: CGRectMake(100, 100, 50, 50))
+        blueSquare.backgroundColor = UIColor.blackColor()
+        view.addSubview(blueSquare)
+        
+        let redSquare = UIView(frame: CGRectMake(300, 400, 50, 50))
+        redSquare.backgroundColor = UIColor.redColor()
+        view.addSubview(redSquare)
+        
+        addDynamicBehavior([blueSquare, redSquare])
+    }
+    
+    
+    func addDynamicBehavior(array: [UIView])
+    {
+        let dynamicBehavior = UIDynamicItemBehavior(items: array)
+        dynamicBehavior.density = 1.0
+        dynamicBehavior.friction = 0.0
+        dynamicBehavior.resistance = 0.0
+        dynamicBehavior.elasticity = 1.0
+        dynamicBehavior.addChildBehavior(dynamicBehavior)
+        
+        let pushBehavior = UIPushBehavior(items: array, mode: .Instantaneous)
+        pushBehavior.magnitude = 1.0
+        pushBehavior.pushDirection = CGVectorMake(0.5, 0.5)
+        
+        let collisionBehavior = UICollisionBehavior(items: array)
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        collisionBehavior.collisionMode = .Everything
+        collisionBehavior.collisionDelegate = self
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
