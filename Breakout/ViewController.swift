1c121bbc8589e55e8f3a7178d00f7d6b0a682180
student3 <student3@jh-mbair-158-10.d214.org>
2016-04-20 14:47:44 -0500

    lk

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -9,23 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController, UICollisionBehaviorDelegate {
-    
-    var dynamicAnimator = UIDynamicAnimator()
+   var dynamicAnimator = UIDynamicAnimator()
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dynamicAnimator = UIDynamicAnimator(referenceView: view)
-
+        createBlock()
     }
     
-    func setUpViews(){
-        let blueSquare = UIView(frame: CGRectMake(100, 100, 50, 50))
-        blueSquare.backgroundColor = UIColor.blackColor()
-        view.addSubview(blueSquare)
-        
-        let redSquare = UIView(frame: CGRectMake(300, 400, 50, 50))
-        redSquare.backgroundColor = UIColor.redColor()
-        view.addSubview(redSquare)
+    func createBlock(){
+        let heightOfBlock = 50
+        let lengthOfBlock = 0
+//        //let blueSquare = UIView(frame: CGRectMake(100, 100, 50, 50))
+//        blueSquare.backgroundColor = UIColor.blackColor()
+//        view.addSubview(blueSquare)
+//        
+//        let redSquare = UIView(frame: CGRectMake(300, 400, 50, 50))
+//        redSquare.backgroundColor = UIColor.redColor()
+//        view.addSubview(redSquare)
         
         addDynamicBehavior([blueSquare, redSquare])
     }
@@ -33,16 +36,17 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     func addDynamicBehavior(array: [UIView])
     {
-        let dynamicBehavior = UIDynamicItemBehavior(items: array)
-        dynamicBehavior.density = 1.0
-        dynamicBehavior.friction = 0.0
-        dynamicBehavior.resistance = 0.0
-        dynamicBehavior.elasticity = 1.0
-        dynamicBehavior.addChildBehavior(dynamicBehavior)
+        let dynamicItemBehavior = UIDynamicItemBehavior(items: array)
+        dynamicItemBehavior.density = 1.0
+        dynamicItemBehavior.friction = 0.0
+        dynamicItemBehavior.resistance = 0.0
+        dynamicItemBehavior.elasticity = 1.0
+        dynamicAnimator.addBehavior(dynamicItemBehavior)
         
         let pushBehavior = UIPushBehavior(items: array, mode: .Instantaneous)
         pushBehavior.magnitude = 1.0
         pushBehavior.pushDirection = CGVectorMake(0.5, 0.5)
+        dynamicAnimator.addBehavior(pushBehavior)
         
         let collisionBehavior = UICollisionBehavior(items: array)
         collisionBehavior.translatesReferenceBoundsIntoBoundary = true
